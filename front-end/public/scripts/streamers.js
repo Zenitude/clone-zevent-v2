ca78805d2dc61a47085b5c24f9b9a92a9734cd51
Zenitude <mollet.rudy@gmail.com>
2023-08-25 16:38:01 +0200

    css list streamers, historics, games, users

--- front-end/public/scripts/streamers.js
+++
@@ -1,8 +0,0 @@
-// Back previous page
-if(document.querySelector('.containerDeleteStreamer')) {
-    const buttonBack = document.querySelector('.back');
-    buttonBack.addEventListener('click', (e) => {
-        e.preventDefault();
-        window.history.back();
-    })
-}
