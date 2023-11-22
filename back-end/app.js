b2944043b1a806bbbe0dde9f8df78b36446cf266
Rudy Mollet <mollet.rudy@gmail.com>
2023-11-22 15:00:46 +0100

    add cors

--- back-end/app.js
+++ back-end/app.js
@@ -6,6 +6,7 @@ const methodOverride = require("method-override");
 const session = require("express-session");
 const multer = require("multer");
 const cookieParser = require("cookie-parser");
+const cors = require('cors');
 const { dateMiddleware } = require("./utils/middlewares/date");
 const connectDb = require("./utils/database/database");
 
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cors());
 app.use(methodOverride("_method"));
 app.use(multer().any());
 app.use(cookieParser());
